55641cd46ef10a26a65eec3b728e8dcce7a98940
Faizan Iftikhar <faizan.iftikhar@careem.com>
2020-05-30 17:22:35 +0400

    Spring initializer project for e-commerce project (Maha)

---
+++ src/main/java/com/ecommerce/maha/MahaApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.maha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MahaApplication.class, args);
+    }
+
+}
