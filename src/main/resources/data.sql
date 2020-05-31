74e7f1b609c4e1316e93476b022aabbf3740e7a8
Faizan Iftikhar <faizan.iftikhar@careem.com>
2020-05-31 21:45:17 +0400

    Added related dependencies and configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+
+
+insert into watch
+values (1, 100, '001', 'Rolex'),
+       (2, 80, '002', 'Michael Kors'),
+       (3, 50, '003', 'Swatch'),
+       (4, 30, '004', 'Casio');
+
+insert into discount
+values (1, 200, 3, 1),
+       (2, 120, 2, 2);
